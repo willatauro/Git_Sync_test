4a4db5fc7230fc059f65e788e71e768733d235eb
willatauro <willatauro@gmail.com>
2020-07-31 23:31:06 -0400

    30jul

---
+++ mosh_practise/app.py
@@ -0,0 +1,98 @@
+'''
+
+print('Wilander '*5)
+
+price = 10
+rating = 4.9
+is_published = False 
+name = "Wilander is awesome"
+ 
+
+if is_published:
+    print(name)
+
+
+# Print patient name, age and whether he is new patient
+
+name = 'Wilander'
+age = 32
+is_patient = False 
+
+
+print (name)
+print(age)
+print(is_patient)
+
+
+ 
+
+name = input('Whats is your name? ' )
+lastname = input('Whats your last name? ')
+print ('Hi '+name+' '+lastname)
+
+birth_year = int(input('Birth year: '))
+age = 2020 - birth_year
+
+print(type(age))
+ 
+
+weight = input('Enter your weight in lb ')
+kg = float(weight)* 0.45
+print(kg)
+
+
+ 
+#strings , slicing
+
+course = 'Python for beginers'
+
+print(course)
+print(course[1:-1])
+ 
+name = 'alisa'
+print(name[1:-2])
+ 
+# formatted string
+name = 'Wilander'
+last = 'Tauro'
+
+msg = f'{name} {last} is an awesome coder'
+msg1 = name + ' ' +last + ' is awesome'
+print(msg)
+print(msg1)
+ 
+
+name ='Wilander'
+
+print(len(name))
+
+print(name.upper())
+print(name)
+
+
+name = 'Wilander is awesome'
+print(name.find('m'))
+print(name.replace('Wilander','Alisa'))
+print('Wilander' in name)
+
+
+x= 20
+x+=3
+
+print(x)
+
+y = 10 + 3 *2 ** 2
+print(y)
+
+z = (2+3) * 10 - 3
+print(z)
+ 
+
+x= 2.9
+print(round(x))
+
+'''
+
+#import math
+
+
